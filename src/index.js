75efc2498f85ee284e0fdbbdba6c4ba8577ee128
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-05-10 16:48:43 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Description
+// We're going to put to good use our knowledge of fetch by creating a full client in the browser! To achieve this, you'' 
+// have to set up all the interactions with the server
+
+// Instructions
+// - Use this template as a starting point. Make sure you download it into your laptop => https://codesandbox.io/s/instabool-template-cwhm7?file=/index.html
+// - Set up your json-server using the files in the db folder; You must start the server on your local machine, 
+//   using this exact command from the terminal on the root of your project folder:  json-server --watch db/db.json --routes db/routes.json 
+
+// - Dynamically render the posts using the card HTML portion that you'll find in the index.html
+// - Try to use the same CSS classes to achieve the desired look
+
+
+// Tips
+// - Make some requests to your server and inspect the response, so you can check the data structure before start coding
+// - Focus first on render the data onto your page
+// - Try to think which kind of HTTP method you should use on each occasion
+// - Try to use function scopes to your advantage
